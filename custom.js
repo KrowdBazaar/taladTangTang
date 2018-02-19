3355a67e23e8537529b5f9e24a0213c2581d98a0
kbazaar <35330758+kbazaar@users.noreply.github.com>
2018-02-19 12:03:04 +0700

    Update custom.js

--- custom.js
+++ custom.js
@@ -2,6 +2,7 @@ var newDiv = document.createElement("div");
 var chatClass = document.createAttribute("class");
 var page_id = document.createAttribute("page_id");
 var ref = document.createAttribute("ref");
+var free_product_match = "฿0.00";
 chatClass.value = "fb-customerchat";
 page_id.value = "173416783249972";
 ref.value = "website_taladtangtang";
@@ -34,9 +35,9 @@ setTimeout(function () {
 }(document, 'script', 'facebook-jssdk'));
 }, 0);
 
-$(document).ready(function(){
-  console.log($('li.active').attr('class'));
-});
+$("a.grid-product__image").click(function(e) {
+if($('.ecwid-productBrowser-price').text() == free_product_match)console.log("Is Free Product");
+});  
 // Ecwid.OnPageLoaded.add(function(page) {
 //   widgets = Ecwid.getInitializedWidgets();
 //   // if storefront widget is present on this page

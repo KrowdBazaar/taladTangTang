528f4840fe99dc31bf11612bb35982878aa5c378
kbazaar <35330758+kbazaar@users.noreply.github.com>
2018-02-23 06:55:15 +0700

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,14 +1,20 @@
-Ecwid.OnPageLoaded.add(function(page){
-  console.log("Current page is of type: " + page.type);
-  console.log($('.ecwid-productBrowser-price').text());
-});
-console.log($('.ecwid-productBrowser-price').text());
-
 var newDiv = document.createElement("div");
 var chatClass = document.createAttribute("class");
 var page_id = document.createAttribute("page_id");
 var ref = document.createAttribute("ref");
 var free_product_match = "฿0.00";
+
+//FREE PRODUCT DISPLAY WITHOUT 
+Ecwid.OnPageLoaded.add(function(page){
+  if($('.ecwid-productBrowser-price').text() == free_product_match){
+    console.log("Is Free Product");
+    $('.ecwid-btn--addToBag').hide();    
+    $('.ecwid-productBrowser-details-qtyPanel').hide();
+    $('.ecwid-productBrowser-sku').hide();
+    $('.ecwid-productBrowser-price').hide();
+  }
+});
+
 chatClass.value = "fb-customerchat";
 page_id.value = "173416783249972";
 ref.value = "website_taladtangtang";
